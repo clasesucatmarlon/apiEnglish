8601825dff2d3042000c636e00e7acbef174827f
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-02-18 21:31:06 -0500

    Primera version

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 // Archivo de configuración que retorna un objeto: puerto y base de datos usada 
+// DB: process.env.DB || 'mongodb://localhost:27017/api-english-verb-reg'
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: process.env.DB || 'mongodb://localhost:27017/api-english-verb-reg'
-    /* DB: process.env.DB || 'mongodb+srv://marlon:marlon@cluster0.znqfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' */
+    DB: process.env.DB || 'mongodb+srv://marlon:marlon@cluster0.znqfr.mongodb.net/api-english-verb-reg?retryWrites=true&w=majority'
 }
